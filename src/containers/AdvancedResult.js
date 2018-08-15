df2b35e5557fb977a71fcd8a31b004acdd4d9eb4
BladeMasterZ <xianz2@andrew.cmu.edu>
2018-08-15 01:22:48 -0400

    revert back2

--- src/containers/AdvancedResult.js
+++ src/containers/AdvancedResult.js
@@ -5,7 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import DataGraph from '../components/DataGraph'
 import DataList from '../components/DataList'
-
+import PatentList from '../components/PatentList'
 
 
 class AdvancedResult extends Component {
